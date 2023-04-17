e8964932d24c081c4af3472ddf41c5296aba20bd
Jahongir Ortiqboyev <108238210+OrtiqboyevJahongir@users.noreply.github.com>
2023-04-17 16:44:17 +0500

    first commit

---
+++ public/js/client.js
@@ -0,0 +1,76 @@
+const socket =io()
+
+let username;
+let chats = document.querySelector(".chats")
+let users_list = document.querySelector(".users-list")
+let users_count = document.querySelector(".users-count")
+let msg_send = document.querySelector("#user-send")
+let user_msg = document.querySelector("#user-msg")
+
+do{
+    username = prompt("enter your name ");
+}while(!username);
+
+socket.emit("new-user-joined" , username);
+
+socket.on("user-connected",(socket_name)=>{
+    userJoinLeft(socket_name,'joined');
+
+})
+
+function userJoinLeft(name , status){
+    let div = document.createElement("div")
+    div.classList.add("user-join");
+    let content = `<p> <b> ${name} </b>  ${status} the chat </p>`
+    div.innerHTML = content
+    chats.appendChild(div)
+    chats.scrollTop = chats.scrollHeight
+}
+
+socket.on("user-disconnected" , (user)=>{
+    userJoinLeft(user,"left")
+})
+
+
+socket.on("user-list" , (users)=>{
+    users_list.innerHTML = ""
+    users_arr = Object.values(users)
+    for(let i=0 ; i<users_arr.length ; i++){
+        let p=document.createElement("p")
+        p.innerText = users_arr[i];
+        users_list.appendChild(p);
+    }
+    users_count.innerHTML = users_arr.length
+})
+msg_send.addEventListener("click" , (e)=>{
+      let data={
+        user:username,
+        msg:user_msg.value
+    }
+    if(user_msg.value!=""){
+        appendMessage(data,"outgoing");
+        socket.emit("message" , data);
+        user_msg.value = "";
+
+}
+    
+  
+})
+
+
+function appendMessage(data , status){
+    let div= document.createElement("div")
+    div.classList.add("message" , status);
+    let content=`
+        <h5>${data.user}</h>
+        <p>${data.msg}</p>
+    `
+    div.innerHTML=content;
+    chats.appendChild(div)
+
+    chats.scrollTop = chats.scrollHeight
+}
+
+socket.on("message" , (data)=>{
+    appendMessage(data,"incoming")
+})
